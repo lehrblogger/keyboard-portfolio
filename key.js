42bee82c95a8ecd11c6ddbee4ac0c0668735f920
Steven Lehrburger <lehrblogger.com>
2008-10-27 15:47:16 -0400

    initial files from previous assignments
    More information can be found at:
      http://lehrblogger.com/2008/10/03/gui-design-in-ajax-project-idea/
      http://lehrblogger.com/2008/10/17/gui-design-in-ajax-assorted-assignments-from-hw5-hw6/
      http://lehrblogger.com/2008/10/21/gui-design-in-ajax-keyboard-progress/

---
+++ key.js
@@ -0,0 +1,90 @@
+// a separate file for the Key class, since the other one was getting long
+var Key = new Class ( 
+{
+	initialize: function (letter, row, col, originX, originY) {
+		this.letter = letter;
+		this.row = row;
+		this.col = col;
+		this.originX = originX;
+		this.originY = originY;
+		this.locMorph = new Fx.Morph(this.element, {});		// declare these here so we can cancel them easily
+		this.sizeMorph = new Fx.Morph(this.element, {});
+		
+		this.element = new Element('div', {
+			'id':this.letter,
+			'html':this.letter
+		});
+		
+		// Listen for clicks
+	    this.element.addEvent('click', function(e) {
+	      	increaseCurrentSize();
+			zoomOnKey(this.letter.toLowerCase());
+	    }.bind(this));
+	
+		this.element.addClass('key');
+		
+		this.updateSize('keySmall', false);
+		this.setSizes(this.element.getStyle('width').toInt());	//we can use this because the previous call isn't tweening and finishes by the time it gets here
+		this.updateLoc(0, 0, false);
+		
+		document.body.grab(this.element);
+	},
+	
+	// updates the size of the key - 
+ 	updateSize: function(newSize, dimension, useTween) {
+		this.setSizes(dimension);	// this has to be passed because we won't know what we are tweeing to until we get there,
+									// and updateLoc will need that information immediately
+		if (useTween) {		
+			this.sizeMorph.cancel();// cancel the old morph wherever it is, since now there is a new destination
+			this.sizeMorph = new Fx.Morph(this.element, {duration: 500, fps: 24, transition: Fx.Transitions.linear});
+			this.sizeMorph.addEvent('complete', function(e) {
+	     		this.updateClasses(newSize);	// wait till it is complete to actually update the classes
+			}.bind(this));
+			this.sizeMorph.start('.' + newSize);
+ 		} else {
+			this.updateClasses(newSize);		// we don't want to use the tween for the initial setup, and this is useful for debugging
+		}
+	},
+
+	// this removes the old size class, and adds the new one
+	// note those styles also need to be removed because the tween DOES NOT ADD A CLASS and only adds the styles in that class
+	updateClasses: function(newSize) {
+		this.element.removeClass('keySmall');
+		this.element.removeClass('keyMedium');
+		this.element.removeClass('keyLarge');
+		this.element.removeClass('keyImage');
+		//this.element.removePropety('style');
+		this.element.setStyle('border', "");
+		this.element.setStyle('width', "");
+		this.element.setStyle('height', "");
+		this.element.setStyle('font', "");
+
+		this.element.addClass(newSize);
+	},
+	
+	// updates the location of the key, as specified by the left and top CSS sttyles
+	updateLoc: function(offsetX, offsetY, useTween) {
+	 	this.originX += offsetX;	// the location will be dependent on where the current key is, so these offsets should be passed
+	  	this.originY += offsetY;
+		var targetX = this.originX + ((this.size + this.buffer) * this.col);
+		var targetY = this.originY + ((this.size + this.buffer) * this.row);
+		
+		if (useTween) {
+			this.locMorph.cancel(); //why not link: 'cancel'?
+			this.locMorph = new Fx.Morph(this.element, {duration: 500, fps: 24, transition: Fx.Transitions.linear});
+			this.locMorph.start({
+		    	'left': targetX,
+		    	'top': targetY
+			});
+		} else {
+			this.element.setStyle('left', targetX);	// agian, useful for debugging
+			this.element.setStyle('top', targetY);
+		}
+	},
+	
+	// a simple function to update the Key's size info
+	setSizes: function(dimension) {
+		this.size = dimension;
+		this.buffer = this.size / 5;
+	}
+});
---
+++ keyboard.js
@@ -0,0 +1,104 @@
+var rowArray = new Array(new Array("Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"),
+				         new Array("A", "S", "D", "F", "G", "H", "J", "K", "L"),
+			 	         new Array("Z", "X", "C", "V", "B", "N", "M")
+			   )
+var allKeyObjects = new Array();
+var currentKey;	// will be initialized in init()
+var currentSize = 'keySmall';
+var keySize;
+var keyBuffer;
+
+function init() {	
+	updateCurrentKeySize();
+	addKeyObjectsAndEvents();
+	updateCurrentKeyByLetter(rowArray[0][0].toLowerCase());
+	
+	// add one last event for the spacebar
+	window.addEvent( 'keydown', function(e) {
+     	var evt = new Event(e);
+      	if(evt.key == 'space') { // FIX THIS	
+			decreaseCurrentSize();
+			zoomOnKey(evt.key);
+      	}
+    });
+}
+// builds an object and an keypress event for each key
+function addKeyObjectsAndEvents() {
+	for (var i = 0; i < rowArray.length; i++) {
+		for (var j = 0; j < rowArray[i].length; j++) {
+			allKeyObjects.push(new Key(rowArray[i][j], i, j, keyBuffer, keyBuffer));
+			
+			window.addEvent( 'keydown', function(e) {
+		     	var evt = new Event(e);
+		      	if(evt.key == this.toLowerCase()) {
+					increaseCurrentSize();
+					zoomOnKey(evt.key);
+		      	}
+		    }.bind(rowArray[i][j]));
+		}
+	}	
+}
+
+// zooms around one key - in or out depends on if the current size was just increased or decreased
+function zoomOnKey(letter) {
+	//console.log(currentSize + " " + letter);
+
+	if (currentKey.letter != letter) 	updateCurrentKeyByLetter(letter);	// we only need to update the current letter if it's different (spacebar and that letter keep it the same)
+	
+	// these offsets will be dependent on the *NEW* keySize and keyBuffer, but the currentKey's origin and row/col won't change
+    var offsetX = keyBuffer - (currentKey.originX + ((keySize + keyBuffer) * currentKey.col));
+    var offsetY = keyBuffer - (currentKey.originY + ((keySize + keyBuffer) * currentKey.row));	
+
+	// for all the other keys, update the size and location using the tweens
+	for (var i = 0; i < allKeyObjects.length; i++) {
+		allKeyObjects[i].updateSize(currentSize, keySize, true);
+		allKeyObjects[i].updateLoc(offsetX, offsetY, true);
+	}
+}
+// goes through all of the key objects, and find the one with the same letter as is passed
+function updateCurrentKeyByLetter(letter) {
+	for (var i = 0; i < allKeyObjects.length; i++) {
+		if (allKeyObjects[i].letter.toLowerCase() == letter) {
+			currentKey = allKeyObjects[i];
+		} 
+	}
+}
+
+// increase the current key size and update the current size
+function increaseCurrentSize() {
+	if (currentSize == 'keySmall') {
+		currentSize = 'keyMedium';
+	} else	if (currentSize == 'keyMedium') {
+		currentSize = 'keyLarge';
+	} else	if (currentSize == 'keyLarge') {
+		currentSize = 'keyImage';			
+	}	
+	updateCurrentKeySize();
+}
+// decrease the current key size and update the current size
+function decreaseCurrentSize() {
+	if (currentSize == 'keyImage') {
+		currentSize = 'keyLarge';
+	} else	if (currentSize == 'keyLarge') {
+		currentSize = 'keyMedium';
+	} else	if (currentSize == 'keyMedium') {
+		currentSize = 'keySmall';			
+	}
+	updateCurrentKeySize();
+}
+// creates a key element with the current class, gets the size, and deletes it. there must be a better way to do this
+function updateCurrentKeySize() {
+	var tempElem = new Element('div', {
+		'class': currentSize
+	});
+	
+	keySize = tempElem.getStyle('width').toInt();
+	keyBuffer = keySize / 5;
+	
+	tempElem.removeClass(currentSize);
+	delete tempElem;
+}
+
+
+
+
